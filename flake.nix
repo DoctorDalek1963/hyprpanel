821c70b99d99d6a81cf33b19262d2d383f7048cf
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-12-22 23:41:31 +0000

    Add devShell, justfile, and .envrc

--- flake.nix
+++ flake.nix
@@ -24,7 +24,66 @@
       "aarch64-linux"
     ];
     forEachSystem = nixpkgs.lib.genAttrs systems;
+
+    mkExtraPackages = system: pkgs: [
+      ags.packages.${system}.agsFull
+      astal.packages.${system}.tray
+      astal.packages.${system}.hyprland
+      astal.packages.${system}.io
+      astal.packages.${system}.apps
+      astal.packages.${system}.battery
+      astal.packages.${system}.bluetooth
+      astal.packages.${system}.mpris
+      astal.packages.${system}.network
+      astal.packages.${system}.notifd
+      astal.packages.${system}.powerprofiles
+      astal.packages.${system}.wireplumber
+      pkgs.fish
+      pkgs.typescript
+      pkgs.libnotify
+      pkgs.dart-sass
+      pkgs.fd
+      pkgs.btop
+      pkgs.bluez
+      pkgs.libgtop
+      pkgs.gobject-introspection
+      pkgs.glib
+      pkgs.bluez-tools
+      pkgs.grimblast
+      pkgs.gpu-screen-recorder
+      pkgs.brightnessctl
+      pkgs.gnome-bluetooth
+      (pkgs.python3.withPackages (python-pkgs:
+        with python-pkgs; [
+          gpustat
+          dbus-python
+          pygobject3
+        ]))
+      pkgs.matugen
+      pkgs.hyprpicker
+      pkgs.hyprsunset
+      pkgs.hypridle
+      pkgs.wireplumber
+      pkgs.networkmanager
+      pkgs.upower
+      pkgs.gvfs
+      pkgs.swww
+      pkgs.pywal
+    ];
   in {
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        nativeBuildInputs =
+          [
+            pkgs.just
+            pkgs.nix-output-monitor
+          ]
+          ++ (mkExtraPackages system pkgs);
+      };
+    });
+
     packages = forEachSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
@@ -35,51 +94,7 @@
         entry = "app.ts";
 
         # Additional libraries and executables to add to the GJS runtime
-        extraPackages = [
-          ags.packages.${system}.agsFull
-          astal.packages.${system}.tray
-          astal.packages.${system}.hyprland
-          astal.packages.${system}.io
-          astal.packages.${system}.apps
-          astal.packages.${system}.battery
-          astal.packages.${system}.bluetooth
-          astal.packages.${system}.mpris
-          astal.packages.${system}.network
-          astal.packages.${system}.notifd
-          astal.packages.${system}.powerprofiles
-          astal.packages.${system}.wireplumber
-          pkgs.fish
-          pkgs.typescript
-          pkgs.libnotify
-          pkgs.dart-sass
-          pkgs.fd
-          pkgs.btop
-          pkgs.bluez
-          pkgs.libgtop
-          pkgs.gobject-introspection
-          pkgs.glib
-          pkgs.bluez-tools
-          pkgs.grimblast
-          pkgs.gpu-screen-recorder
-          pkgs.brightnessctl
-          pkgs.gnome-bluetooth
-          (pkgs.python3.withPackages (python-pkgs:
-            with python-pkgs; [
-              gpustat
-              dbus-python
-              pygobject3
-            ]))
-          pkgs.matugen
-          pkgs.hyprpicker
-          pkgs.hyprsunset
-          pkgs.hypridle
-          pkgs.wireplumber
-          pkgs.networkmanager
-          pkgs.upower
-          pkgs.gvfs
-          pkgs.swww
-          pkgs.pywal
-        ];
+        extraPackages = mkExtraPackages system pkgs;
       };
     });
   };
---
+++ justfile
@@ -0,0 +1,13 @@
+_default:
+	@just --list
+
+alias r := run
+alias b := build
+
+# run the bar
+run:
+	ags run app.ts
+
+# build the default bar
+build:
+	nom build {{justfile_directory()}}#hyprpanel
